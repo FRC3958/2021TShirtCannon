a04eb72f57ea7feacf3007ac43c0ce3a5c4d7562
Jelle <w.lenig@icloud.com>
2021-11-17 16:53:41 -0500

    initial

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Relay.Value;
+import frc.robot.commands.Driving;
+// import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveTrain;
+// import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+ 
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  // private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  private final DriveTrain m_DT = new DriveTrain(); 
+  private final XboxController m_XC = new XboxController(Constants.XboxControllerPort);
+  private final Driving m_Driving = new Driving(m_DT, m_XC);
+  private final Relay m_solenoidrelay = new Relay(Constants.solenoidRelayPort); 
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    m_DT.setDefaultCommand(m_Driving);
+
+    new JoystickButton(m_XC, Constants.ForwardButton) 
+      .whenPressed(() -> m_solenoidrelay.set(Value.kOn))
+      .whenReleased(() -> m_solenoidrelay.set(Value.kOff));
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  // public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    // return m_autoCommand;
+  }
+// }
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+
+
+public class DriveTrain extends SubsystemBase {
+
+  // Parts of the Robot
+  private static WPI_TalonSRX frontright = new WPI_TalonSRX(Constants.frontrightmotor); 
+  private static WPI_TalonSRX frontleft = new WPI_TalonSRX(Constants.frontleftmotor);
+  private static WPI_TalonSRX backright = new WPI_TalonSRX(Constants.backrightmotor);
+  private static WPI_TalonSRX backleft = new WPI_TalonSRX(Constants.backleftmotor); 
+
+  // Differentiating the front right and left motors
+  public static DifferentialDrive D = new DifferentialDrive(frontright, frontleft);
+
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+
+    // Setting neutral modes for the parts
+    frontright.setNeutralMode(NeutralMode.Coast);
+    frontleft.setNeutralMode(NeutralMode.Coast);
+    backright.setNeutralMode(NeutralMode.Coast);
+    backleft.setNeutralMode(NeutralMode.Coast);
+
+    //Interverting the left and right with the Differential Drive
+    /* backleft.setInverted(InvertType.None);
+    frontleft.setInverted(InvertType.FollowMaster); */
+    backright.setInverted(InvertType.InvertMotorOutput);
+    frontright.setInverted(InvertType.FollowMaster); 
+    
+    // to set the back wheels to follow the front wheels
+    backright.follow(frontright);
+    backleft.follow(frontleft);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void drive(double forward, double turn) {
+    D.arcadeDrive(turn, forward);
+    
+  }
+    
+}
